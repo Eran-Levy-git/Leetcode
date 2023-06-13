c1d861a37b1610d93015415c6604319f2f232dcb
Eran-Levy-git <74136274+Eran-Levy-git@users.noreply.github.com>
2023-06-13 10:35:28 +0300

    Add files via upload

---
+++ fizzBuzz.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                answer.append('FizzBuzz')
+            elif i % 3 == 0:
+                answer.append('Fizz')
+            elif i % 5 == 0:
+                answer.append("Buzz")
+            else:
+                answer.append(str(i))
+        return answer
+
+
+"""
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+"""
+
+# Test Cases
+test_cases = [3, 8, 12]
+
+sol = Solution()
+
+for i, accounts in enumerate(test_cases):
+    result = sol.fizzBuzz(accounts)
+    print(f"Test Case {i + 1}: {result}")
