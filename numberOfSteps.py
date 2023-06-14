b3fe7a1bb13386c098c09702083065d1979c12a9
Eran-Levy-git <74136274+Eran-Levy-git@users.noreply.github.com>
2023-06-14 12:54:02 +0300

    Update numberOfSteps.py

--- numberOfSteps.py
+++ numberOfSteps.py
@@ -8,10 +8,4 @@ class Solution:
                 num -= 1
             counter += 1
         return counter
-
-
-"""
-Given an integer num, return the number of steps to reduce it to zero.
-
-In one step, if the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
-"""
+        
