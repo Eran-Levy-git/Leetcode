66a9afa790548ebd3f8a0f36c2624cb0fd3754b5
Eran-Levy-git <74136274+Eran-Levy-git@users.noreply.github.com>
2023-06-14 12:54:18 +0300

    Update fizzBuzz.py

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -15,16 +15,6 @@ class Solution:
                 answer.append(str(i))
         return answer
 
-
-"""
-Given an integer n, return a string array answer (1-indexed) where:
-
-answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
-answer[i] == "Fizz" if i is divisible by 3.
-answer[i] == "Buzz" if i is divisible by 5.
-answer[i] == i (as a string) if none of the above conditions are true.
-"""
-
 # Test Cases
 test_cases = [3, 8, 12]
 
