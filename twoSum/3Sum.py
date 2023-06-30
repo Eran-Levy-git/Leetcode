27f7b031adcf808b7cead95c8e591ac9dcee3a43
Eran <eran.levi3@mail.huji.ac.il>
2023-06-30 20:20:20 +0300

    modified:   twoSum/3Sum.py

--- twoSum/3Sum.py
+++ twoSum/3Sum.py
@@ -24,6 +24,17 @@ def threeSum(nums: List[int]) -> List[List[int]]:
     return l
 
 
+# def threeSum(self, nums: List[int]) -> List[List[int]]:
+#     nums.sort()
+#     result = []
+#     for i in range(len(nums)):
+#         for j in range(i + 1, len(nums)):
+#             for k in range(j + 1, len(nums)):
+#                 if nums[i] + nums[j] + nums[k] == 0:
+#                     result.append([nums[i], nums[j], nums[k]])
+#     return result
+
+
 nums = [-1, 0, 1, 2, -1, -4]
 
 result = threeSum(nums)
