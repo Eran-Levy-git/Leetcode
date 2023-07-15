6d86bf8cd273d06bd1b2ce789760551ea154f932
Eran <eran.levi3@mail.huji.ac.il>
2023-07-15 11:25:02 +0300

    new file:   power function.py

---
+++ power function.py
@@ -0,0 +1,42 @@
+def power1(base, pow):
+    return base**pow
+
+
+def power2(base, pow):
+    result = 1
+    for _ in range(pow):
+        result *= base
+    return result
+
+
+def power3(base, pow):
+    result = 1
+    while pow > 0:
+        if pow % 2 == 1:
+            result *= base
+            pow -= 1
+        pow //= 2
+        base *= base
+    return result
+
+
+def power4(base, pow):
+    if pow == 0:
+        return 1
+    elif pow < 0:
+        return 1 / power3(base, -pow)
+    elif pow % 2 == 1:
+        return base * power3(base, pow - 1)
+    else:
+        return power3(base * base, pow // 2)
+
+
+result1 = power1(2, 3)
+result2 = power2(2, 3)
+result3 = power3(2, 3)
+result4 = power4(2, 3)
+
+print(f"Result 1 is: {result1}")
+print(f"Result 2 is: {result2}")
+print(f"Result 3 is: {result3}")
+print(f"Result 4 is: {result4}")
